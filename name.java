12dff3597ff4bf92593bb2333e2ba9758d558dad
Thanikai <thanikai1996@gmail.com>
2021-04-25 12:05:19 +0530

    "fuckin commit"

---
+++ name.java
@@ -0,0 +1,15 @@
+/* Java Program Example - Print String */
+		
+import java.util.Scanner;
+
+public class JavaProgram
+{
+   public static void main(String args[])
+   {
+      String str;
+	  
+      System.out.print("Enter Your First Name : ");
+	  
+      System.out.print("Hello, This is Arasu");
+   }
+}
